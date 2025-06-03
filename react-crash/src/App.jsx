6fc21fe6855ff38d13ce6edd8c1eac1e6b5db743
Leonardo Céspedes <lecespedes@estudiantec.cr>
2025-06-03 13:14:05 -0600

    Header Completed

--- react-crash/src/App.jsx
+++ react-crash/src/App.jsx
@@ -1,19 +1,22 @@
-const Card = ({title}) => {
-    return (
-        <div className="card">
-            <h2>{title}</h2>
-        </div>
-    )
-}
+import { useState } from "react"
+import Search from "./components/search"
 
 const App = () => {
-    return (
-        <div className="card-container">
-            <Card title="Star Wars"/>
-            <Card title="Avatar"/>
-            <Card title="The Lion King"/>
+    const [searchTerm, setSearchTerm] = useState('');
+
+  return (
+    <main>
+        <div className="pattern"/>
+        <div className="wrapper">
+            <header>
+                <img src="./hero.png" alt="Hero Banner" />
+                <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+            </header>
+
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </div>
-    )
+    </main>
+  )
 }
 
 export default App
